04af436f6d1f041d400b4c5422e7cc0bb0cf82bb
Tishacy <tishacy@gmail.com>
2019-04-23 13:47:11 +0800

    Add 5 Longest Palindrome Substring

---
+++ leetcode/5 Longest Palindromic Substring.py
@@ -0,0 +1,41 @@
+"""
+Given a string s, find the longest palindromic substring in s. You may assume that the maximum length of s is 1000.
+
+Example 1:
+Input: "babad"
+Output: "bab"
+Note: "aba" is also a valid answer.
+
+Example 2:
+Input: "cbbd"
+Output: "bb"
+"""
+class Solution(object):
+    def longestPalindrome(self, s):
+        """BruteForce Method
+        :type s: str
+        :rtype: str
+        """
+        # Find all substrings
+        palindromes = []
+        res = ['', 0]
+        for i, cc in enumerate(s):
+            for j, c in enumerate(s[i:]):
+                substr = s[i:i+j+1]
+                length = len(substr)
+                # check if the substring is palindrome
+                if self._isPalindrome(substr, length):
+                    palindromes.append([substr, length])
+                    if len(substr) > res[1]:
+                        res = [substr, length]
+        return res[0]
+
+    def _isPalindrome(self, s, length):
+        for p in range(int(length/2)):
+            q = length-1-p
+            if s[p] != s[q]:
+                return False
+        return True
+
+res = Solution().longestPalindrome("cyyoacmjwjubfkzrrbvquqkwhsxvmytmjvbborrtoiyotobzjmohpadfrvmxuagbdczsjuekjrmcwyaovpiogspbslcppxojgbfxhtsxmecgqjfuvahzpgprscjwwutwoiksegfreortttdotgxbfkisyakejihfjnrdngkwjxeituomuhmeiesctywhryqtjimwjadhhymydlsmcpycfdzrjhstxddvoqprrjufvihjcsoseltpyuaywgiocfodtylluuikkqkbrdxgjhrqiselmwnpdzdmpsvbfimnoulayqgdiavdgeiilayrafxlgxxtoqskmtixhbyjikfmsmxwribfzeffccczwdwukubopsoxliagenzwkbiveiajfirzvngverrbcwqmryvckvhpiioccmaqoxgmbwenyeyhzhliusupmrgmrcvwmdnniipvztmtklihobbekkgeopgwipihadswbqhzyxqsdgekazdtnamwzbitwfwezhhqznipalmomanbyezapgpxtjhudlcsfqondoiojkqadacnhcgwkhaxmttfebqelkjfigglxjfqegxpcawhpihrxydprdgavxjygfhgpcylpvsfcizkfbqzdnmxdgsjcekvrhesykldgptbeasktkasyuevtxrcrxmiylrlclocldmiwhuizhuaiophykxskufgjbmcmzpogpmyerzovzhqusxzrjcwgsdpcienkizutedcwrmowwolekockvyukyvmeidhjvbkoortjbemevrsquwnjoaikhbkycvvcscyamffbjyvkqkyeavtlkxyrrnsmqohyyqxzgtjdavgwpsgpjhqzttukynonbnnkuqfxgaatpilrrxhcqhfyyextrvqzktcrtrsbimuokxqtsbfkrgoiznhiysfhzspkpvrhtewthpbafmzgchqpgfsuiddjkhnwchpleibavgmuivfiorpteflholmnxdwewj")
+print(res)
